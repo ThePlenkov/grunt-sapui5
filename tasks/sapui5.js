a415131e39809ef718bad1f9ae722e9fa2aa83d0
CORPAD\pplenkov <petr.plenkov@booking.com>
2017-07-10 18:04:10 +0200

    initial commit

---
+++ tasks/sapui5.js
@@ -0,0 +1,66 @@
+/*
+ * grunt-sapui5
+ * https://github.com/pplenkov/grunt-sapui5
+ *
+ * Copyright (c) 2017 theplenkov
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  'use strict';
+  grunt.loadNpmTasks('@sap/grunt-sapui5-bestpractice-build');
+
+  var oConfig = grunt.config();
+
+  var mergeWith = require('lodash.mergewith');
+
+  var customizer = function (objValue, srcValue) {
+    if (Array.isArray(objValue)) {
+      return [srcValue].concat(objValue);
+    }
+  };
+
+  mergeWith(oConfig,
+    {
+      openui5_preload: {
+        preloadTmp: {
+          options: {
+            compatVersion: "1.38"
+          },
+          libraries: true
+        }
+      },
+      copy: {
+        copyTmpToDist: {
+          files: [
+            {
+              expand: true,
+              src: '**/Component.js',
+              dest: oConfig.dir.dist,
+              cwd: oConfig.dir.tmpDir
+
+            },
+            {
+              expand: true,
+              src: '**/library.js',
+              dest: oConfig.dir.dist,
+              cwd: oConfig.dir.tmpDir
+
+            }
+          ]
+        }
+      }
+    }, customizer
+  );
+
+  grunt.config("openui5_preload", oConfig.openui5_preload);
+  grunt.config("copy", oConfig.copy);
+
+  grunt.registerTask('default', [
+    'lint',
+    'clean',
+    'build'
+  ]);
+};
